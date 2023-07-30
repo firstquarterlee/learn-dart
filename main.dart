4e48ff2035bdc1e95cb2a899a2c2c4b0a287e804
firstquarterlee <firstquarterlee@gmail.com>
2023-07-30 19:59:56 +0900

    🎾변수 선언 방법

--- main.dart
+++ main.dart
@@ -1,3 +1,19 @@
 void main() {
-  print('hello world');
+  String name = '상현';
+  name = '니꼬';
 }
+
+// var, Strng -> 변수를 선언할때 사용한다. (var & 넣고싶은 타입)
+/* 변수를 만드는 2가지 방법
+
+
+void main() {
+var name = "pizza";       -> 방법 1
+String name = "chicken";  -> 방법 2
+name = "chicken ";
+}
+
+함수나 메소드 내부에 지역변수를 선언할 때는 var를 사용하고
+class에서 변수나 property를 선언할 때는 타입을 지정해준다.
+
+*/
